5e9e1ef3b61600c007d818b42ffc6eb897aa5ebc
Todedonga <guillermo.flores@iest.edu.mx>
2025-08-29 17:52:49 -0600

    Initial Commit

---
+++ Tareas_Actividades/Tareas_ActividadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tareas_ActividadesApp.swift
+//  Tareas_Actividades
+//
+//  Created by win603 on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Tareas_ActividadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tareas_ActividadesTests/Tareas_ActividadesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tareas_ActividadesTests.swift
+//  Tareas_ActividadesTests
+//
+//  Created by win603 on 29/08/25.
+//
+
+import Testing
+@testable import Tareas_Actividades
+
+struct Tareas_ActividadesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
