c410d0d6a93eee9f6165eacad87b7578fa75ccc6
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-05 18:50:12 -0600

    tareas

---
+++ Tareas_Actividades/Actividad2.swift
@@ -0,0 +1,18 @@
+//
+//  Actividad2.swift
+//  Tareas_Actividades
+//
+//  Created by win603 on 29/08/25.
+//
+
+import SwiftUI
+
+struct Actividad2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Actividad2()
+}
---
+++ Tareas_Actividades/Boton.swift
@@ -0,0 +1,18 @@
+//
+//  Boton.swift
+//  Tareas_Actividades
+//
+//  Created by win603 on 05/09/25.
+//
+
+import SwiftUI
+
+struct Boton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Boton()
+}
