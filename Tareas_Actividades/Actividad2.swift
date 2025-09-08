49f1b3880d362522c8d8bf5c9c534d1fe625a26f
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-08 17:49:13 -0600

    aaaa

--- Tareas_Actividades/Actividad2.swift
+++ Tareas_Actividades/Actividad2.swift
@@ -9,7 +9,77 @@ import SwiftUI
 
 struct Actividad2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: true) { //contenido
+        
+            ZStack {
+                
+                HStack {
+                    
+                    VStack {
+                        Image("demonslayer")
+                            .resizable()
+                            .cornerRadius(20)
+                            .frame(width: 380, height: 540)
+                        HStack {
+                            Text("TBC")
+                                .frame(width: 50, height: 30)
+                                .background(Color(hue: 1.0, saturation: 0.004, brightness: 0.895))
+                                .foregroundColor(.gray)
+                                .fontWeight(.bold)
+                                .cornerRadius(3)
+                            Text("155 min")
+                        }
+                        Text("Demon Slayer: Kimetsu no Yaiba Castillo Infinito")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .truncationMode(.middle)
+                            .lineLimit(2)
+                        HStack {
+                            Text("Ver detalle")
+                                .foregroundColor(.blue)
+                                .fontWeight(.bold)
+                            Image(systemName: "info.circle")
+                                .foregroundColor(.blue)
+                                
+                        }
+                    }
+                    
+                    VStack {
+                        Image("armand")
+                            .resizable()
+                            .cornerRadius(20)
+                            .frame(width: 380, height: 540)
+                        HStack {
+                            Text("B")
+                                .frame(width: 30, height: 30)
+                                .background(Color.yellow)
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+                                .cornerRadius(3)
+                            Text("116 min")
+                        }
+                        Text("Armand, Una Acusación Peligrosa")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .truncationMode(.middle)
+                        
+                        HStack {
+                            Text("Ver detalle")
+                                .foregroundColor(.blue)
+                                .fontWeight(.bold)
+                            Image(systemName: "info.circle")
+                                .foregroundColor(.blue)
+                                
+                        }
+                    }
+                    
+                }
+                
+            }
+            .frame(width: 760, height: 700)
+            
+            
+        }
     }
 }
 
--- Tareas_Actividades/Boton.swift
+++ Tareas_Actividades/Boton.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct Boton: View {
+    
+    @State var aumento: CGFloat = 2.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            Image(systemName: "paperplane")
+                .scaleEffect(aumento)
+            Button(action: {
+                withAnimation() {
+                    aumento += 0.5
+                }
+            }) {
+                Text("+")
+                    .padding(10)
+            }
+            .frame(width: 100, height: 50)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+            Button(action: {
+                withAnimation() {
+                    aumento -= 0.5
+                }
+            }) {
+                Text("-")
+                    .padding(10)
+            }
+            .frame(width: 100, height: 50)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
     }
+    
 }
 
 #Preview {
