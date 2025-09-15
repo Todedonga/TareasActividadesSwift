219cc113821b31ee18f4a2ceeec6259f8ef4fe9a
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-15 16:07:13 -0600

    cambios y asi

---
+++ Tareas_Actividades/MyGradientStyle.swift
@@ -0,0 +1,18 @@
+//
+//  MyGradientStyle.swift
+//  Tareas_Actividades
+//
+//  Created by WIN603 on 15/09/25.
+//
+
+import SwiftUI
+
+struct MyGradientStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyGradientStyle()
+}
